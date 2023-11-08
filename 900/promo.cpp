336807090d8932957c4b7b5f9ec3ca2d99bec63e
komplike <romansan.tn@gmail.com>
2023-11-08 23:25:12 +0100

    add solved 900 sets

--- 900/promo.cpp
+++ 900/promo.cpp
@@ -1,20 +1,33 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+// #include <chrono>
 
 using namespace std;
+// int n, q, res[200001];
 
-int n, q, p[200001], x , y, res[200001];
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, q;
     cin >> n >> q;
+    vector<long long> p(n), s(n+1);
+
+    for (auto& x : p) cin >> x;
+    sort(p.begin(),p.end());
+
+    for (int i = 0; i < n; ++i) s[i + 1] = s[i] + p[i];
+
+    while (q--) 
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << s[n - x + y] - s[n - x] << '\n';
+    }
+    /* slow code - initial
     for (int i = 0; i < n; i++)
     {
         cin >> p[i];
     }
-
-    sort(&p[0],&p[n]);
-
     for (int i = 0; i < q; i++)
     {
         cin >> x >> y;
@@ -26,5 +39,5 @@ int main(){
     for (int i = 0; i < q; i++)
     {
         cout << res[i] << "\n";
-    }
+    } */
 }
